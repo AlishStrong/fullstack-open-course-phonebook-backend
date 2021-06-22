d8e1268a17f799bc86d968394791e05560e162a2
Alisher <alisher.aliev@tieto.com>
2021-06-22 10:01:08 +0300

    part3: phonebook-backend 3.1

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const app = express();
 
+const persons = [
+  { 
+    'name': 'Arto Hellas', 
+    'number': '040-123456',
+    'id': 1
+  },
+  { 
+    'name': 'Ada Lovelace', 
+    'number': '39-44-5323523',
+    'id': 2
+  },
+  { 
+    'name': 'Dan Abramov', 
+    'number': '12-43-234345',
+    'id': 3
+  },
+  { 
+    'name': 'Mary Poppendieck', 
+    'number': '39-23-6423122',
+    'id': 4
+  }
+];
+
+const personsEndpoint = '/api/persons';
+
+app.get(personsEndpoint, (req, res) => {
+  res.json(persons);
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Phonebook backend listens on port ${PORT}`));
